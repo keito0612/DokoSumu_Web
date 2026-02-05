908f9524419ac32fe9b4a81a08bd33522b3468cd
keito0612 <iso309532@gmail.com>
2026-02-05 22:36:46 +0900

    supabaseの接続時の不具合を修正。

--- src/config/database.php
+++ src/config/database.php
@@ -95,6 +95,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'require'),
+            'prepared_statements' => false,
         ],
 
         'sqlsrv' => [
